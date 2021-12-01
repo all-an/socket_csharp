047a62c5ad11dea1108c71ec49e21952182619c0
all-an <all.ancapistan@gmail.com>
2021-12-01 09:37:44 -0300

    criando classes

---
+++ Sockets/Sockets/GerenciadorSockets/GerenciadorConexoes.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sockets.GerenciadorSockets
+
+{
+    public class GerenciadorConexoes
+    {
+        private ConcurrentDictionary<>
+    }
+}
