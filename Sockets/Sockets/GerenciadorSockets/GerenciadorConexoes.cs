d3adf1e61381c4eeabe67028da8503ec13fed7dd
all-an <all.ancapistan@gmail.com>
2021-12-02 12:23:15 -0300

    propiedades

--- Sockets/Sockets/GerenciadorSockets/GerenciadorConexoes.cs
+++ Sockets/Sockets/GerenciadorSockets/GerenciadorConexoes.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Threading.Tasks;
 
 namespace Sockets.GerenciadorSockets
@@ -9,6 +10,7 @@ namespace Sockets.GerenciadorSockets
 {
     public class GerenciadorConexoes
     {
-        private ConcurrentDictionary<>
+        private ConcurrentDictionary<string, WebSocket> _conexoes = new ConcurrentDictionary<string, WebSocket>();
+
     }
 }
